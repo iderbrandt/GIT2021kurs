edc5b5bb90ead4ec6cb5867616280280a0e82a2d
iderbrandt <tobias.iderbrandt@hm.com>
2021-02-02 13:19:34 +0100

    Added some code

--- src/com/iderbrandt/Main.java
+++ src/com/iderbrandt/Main.java
@@ -1,8 +1,28 @@
 package com.iderbrandt;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<String> movieList = new ArrayList<>();
+        movieList.add("Rick and morty");
+        movieList.add("South Park");
+        movieList.add("The Mandalorian");
+        movieList.add("Breaking bad");
+        movieList.add("Alternated Carbon");
+
+        String shortest = movieList.get(0);
+
+                    for (String element : movieList) {
+                if (element.length() < shortest.length()) {
+                    shortest = element;
+                }
+            }
+
+            System.out.println(shortest);
+
+
     }
 }
